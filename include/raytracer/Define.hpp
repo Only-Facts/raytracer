3dbf16581cf958db984382daa18f1e17c959f136
Tsukini <mat.d.ecole@gmail.com>
2026-05-01 19:48:45 +0200

    fix(color): change the chunk used for the detection of color on face

--- include/raytracer/Define.hpp
+++ include/raytracer/Define.hpp
@@ -19,13 +19,13 @@ File Description:
     /* limits */
     #define LIGHT_INTENSITY_LIMIT 1e-2 // Kill the ray under this intensity
     #define SDF_COLLINDING_LIMIT 1e-2 // Detect colliding if the SDF is below or equal to that value
-    #define LIGHT_COLOR_LIMIT 2.5 // Radius of the circle to take color on a shape pixel hit point
+    #define LIGHT_COLOR_LIMIT 4 // Radius of the circle to take color on a shape pixel hit point
     #define MAX_VERTICES 3 // Allow point, segment, triangle
 
     /* values */
     #define RENDER_DISTANCE 400 // Kill the ray if the SDF is above or equal to that value (can be redefined in the camera)
     #define EXP_K 2 // Exponential coef for light diminution with distance (1 = slow, 10 = fast)
-    #define NOISE_STRENGTH 0.25f // Strength of the noise on the shape color
+    #define NOISE_STRENGTH 0.2f // Strength of the noise on the shape color
     #define NOISE_SIZE 0.1f // Size of the noise (0.05 = big stain, 0.2 = ~normal, 1 = thin, 2 = point)
     #define LIGHT_RAY 1000000 // Number of rays by light
     #define DEFAULT_LIGHT false // Enable or disable seeing object without light
