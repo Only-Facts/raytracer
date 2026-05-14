82982ef1df73cfd4372bb0f444b6eb962ae99bf4
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-05-14 19:03:23 +0200

    feat: add scene config

--- src/config/loader.rs
+++ src/config/loader.rs
@@ -1,17 +1,32 @@
 use serde::Deserialize;
 
-use crate::ffi::bridge::ObjectBridge;
-
-#[derive(Deserialize)]
-pub struct SceneConfig {
-    pub camera: CameraConfig,
-    pub objects: Vec<ObjectBridge>,
-    pub lights: Vec<LightConfig>,
-}
+use crate::{raytracer::structs::Coord, utils::vector::Vector3};
 
 #[derive(Deserialize)]
 pub struct ObjectConfig {
     pub r#type: String,
     pub position: [f64; 3],
     pub color: [u8; 3],
+}
+
+#[derive(Deserialize)]
+pub struct CameraConfig {
+    pub resolution_x: u32,
+    pub resolution_y: u32,
+    pub fov: f64,
+    pub position: Vector3<Coord>,
+    pub rotation: Vector3<Coord>,
+}
+
+#[derive(Deserialize)]
+pub struct LightConfig {
+    pub r#type: String,
+    pub position: Vector3<Coord>,
+}
+
+#[derive(Deserialize)]
+pub struct SceneConfig {
+    pub camera: CameraConfig,
+    pub objects: Vec<ObjectConfig>,
+    pub lights: Vec<LightConfig>,
 }
