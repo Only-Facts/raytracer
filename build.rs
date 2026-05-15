49607df6eaa318dfe5197e85f1ec9227b07e79c7
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-05-15 20:02:22 +0200

    fix: build.rs

--- build.rs
+++ build.rs
@@ -2,11 +2,12 @@ use cmake::Config;
 
 fn main() {
     println!("cargo::rerun-if-changed=src-cpp/");
+    println!("cargo::rerun-if-changed=include/");
     println!("cargo::rerun-if-changed=CMakeLists.txt");
     println!("cargo::rerun-if-changed=build.rs");
 
     Config::new(".")
-        .define("CMAKE_BUILD_TYPE", "Release")
+        .define("CMAKE_BUILD_TYPE", "Optimized")
         .build_target("all")
         .build();
 }
